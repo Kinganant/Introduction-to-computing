4d0f662c0736997e0f6cb54b6f81dc258abfdedc
Kinganant <97087881+Kinganant@users.noreply.github.com>
2022-01-13 19:36:43 +0530

    Add files via upload

---
+++ Asg1_21105027 Output.py
@@ -0,0 +1,31 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: /Users/anantsawhney/Desktop/asg1_21105027.py ============
+#Q1
+Enter first number -40
+Enter second number -70
+Enter third number - 20
+Average- 43.333333333333336
+#Q2
+Enter the Gross Income to the nearest penny -20000
+Enter the number of Dependents -4
+Your income tax is 0$
+#Q3
+Enter your SID-21105027
+Enter your name-Anant Sawhney
+Enter your Gender-(M,F,U) -  M
+Enter the course name-ECE
+Enter the CGPA-9.2
+[21105027, 'Anant Sawhney', 'M', 'ECE', 9.2]
+#Q4
+Enter the marks in Subject 1 = 95
+Enter the marks in Subject 2 = 67
+Enter the marks in Subject 3 = 89
+Enter the marks in Subject 4 = 96
+Enter the marks in Subject 5 = 78
+[67.0, 78.0, 89.0, 95.0, 96.0]
+#Q5(A)
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+#(B)
+['Red', 'Green', 'White', 'Purple', 'Yellow']

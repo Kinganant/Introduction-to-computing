1edde6781b98d51cc00a3dd0237904fab54e93e4
Kinganant <97087881+Kinganant@users.noreply.github.com>
2022-01-23 00:42:03 +0530

    Add files via upload

---
+++ asg2_21105027 output.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: /Users/anantsawhney/Desktop/Assignment2_21105027.py =========
+#Q1
+Part a 
+Length of Input String = 35
+
+Part b
+egaugnal evitisnes esac a si nohtyP
+
+Part c
+a case sensitive
+
+Part d
+Python is object oriented language
+
+Part e
+10
+
+Part f
+Pythonisacasesensitivelanguage
+
+#Q2
+Hey, Anant Sawhney here! 
+My SID is 21105027 
+I am from ECE and my CGPA is 10
+
+#Q3
+ a&b :  8
+ a|b :  58
+ a^b :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>2 : 0
+
+#Q4
+Enter 1st no. : 12
+Enter 2nd no. : 1
+Enter 3rd no. : 35
+Highest no. = 35
+
+#Q5
+Enter input string: hello
+No
+
+#Q6
+Enter 1st length : 23
+Enter 2nd length : 21
+Enter 3rd length : 18
+Yes

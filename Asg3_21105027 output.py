cf1469345b877eff8edd14db34759294bf5bc6b0
Kinganant <97087881+Kinganant@users.noreply.github.com>
2022-02-12 12:24:01 +0530

    Add files via upload

---
+++ Asg3_21105027 output.py
@@ -0,0 +1,77 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: /Users/anantsawhney/Documents/ASG3_21105027.py ===========
+The solution of Question 1 is:
+
+Enter the string: anant
+The occurence(s) of:
+a is/are 2
+n is/are 2
+t is/are 1
+
+The solution of Question 2 is:
+
+Day - 28
+Month - 02
+Year - 1999
+Next Date is:1/3/1999
+
+The solution of Question 3 is:
+
+Enter the list: 3,9,10
+Output: [(3, 9), (9, 81), (10, 100)]
+
+The solution of Question 4 is:
+
+Enter the grade point of the student: 9
+Your Grade is A and Excellent Performance
+
+The solution of Question 5 is:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+The solution of Question 6 is:
+
+Enter the name of the student: Anant Sawhney
+Enter the SID of Anant Sawhney:21105027
+You have entered 1 values till now
+Do you want to enter more data? y
+Enter the name of the student: Vishav Garg
+Enter the SID of Vishav Garg:21103041
+You have entered 2 values till now
+Do you want to enter more data? n
+
+a. Student Details:  
+The SID of Anant Sawhney is  21105027
+The SID of Vishav Garg is  21103041
+
+b. Student Details (sorted with respect to names):
+The SID of Anant Sawhney is 21105027
+The SID of Vishav Garg is 21103041
+
+c. Student Details (sorted with respect to SIDs):
+The SID of Vishav Garg is 21103041
+The SID of Anant Sawhney is 21105027
+
+d. Enter the SID of the student: 21105027
+The name of student whose SID is 21105027 is Anant Sawhney
+
+The solution of Question 7 is:
+
+Enter the no. of terms of the Fibonacci sequence: 9
+The Fibonacci sequence is as follows:
+0 1 1 2 3 5 8 13 21 The average of the terms of Fibonacci sequence upto 9 is 6.0
+
+The solution of Question 8 is:
+
+a. Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}

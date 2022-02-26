a48ec68ec24d716833b0e0a11fa222002d097f08
Kinganant <97087881+Kinganant@users.noreply.github.com>
2022-02-26 23:48:46 +0530

    Add files via upload

---
+++ Asg4_21105027 output.py
@@ -0,0 +1,84 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: /Users/anantsawhney/Documents/Asg4_21105027.py ===========
+ASSIGNMENT 4
+
+Q1
+SOL 1. :- Solution the problem of tower of Hanoi with three disks.
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+Q2
+ 
+ 
+Enter the number of rows in Pascal's Triangle: 6
+
+Using recursions:
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+Using loops:
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+Q3
+ 
+ 
+Enter the first integer: 4
+Enter the first integer: 3
+Remainder is:  1
+Quotient is:  1
+Both values are non zero
+{5}
+{5}
+{5, 6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{5, 6, 7}
+Immutable set:  frozenset({5, 6, 7})
+Largest value in the set:  7
+Hash value:  7
+
+Q4
+ 
+ 
+Object created
+The name of the student it Anant Sawhney and SID is 21105027.
+Object destroyed
+
+Q5
+ 
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+Q6
+ 
+ 
+Enter the first word: seal
+
+Enter a new meaningful word to test your friendship: sale
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
+
+
+
